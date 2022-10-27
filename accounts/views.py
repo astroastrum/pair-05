cc2ba8987a1974c75c85575d1697d21b4623bdae
JungAh Choi <jungahchoi92@gmail.com>
2022-10-27 16:53:51 +0900

    accounts-login/logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
@@ -7,10 +11,28 @@ def signup(request):
         forms = SignupForm(request.POST)
         if forms.is_valid():
             forms.save()
-            return redirect('reviews:index')
+            return redirect("reviews:index")
     else:
         forms = SignupForm()
     context = {
-        "forms" : forms,
+        "forms": forms,
     }
     return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("reviews:index")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    messages.warning(request, "로그아웃 되었습니다")
+    return redirect("accounts:index")
