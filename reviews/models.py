28260ba6f0dd28e6c7574c6bf111d6247cb9500a
nevertheless0404 <nevertheless.hi@gmail.com>
2022-10-27 17:26:05 +0900

    function:reviews-like_users

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Review(models.Model):
-  title = models.CharField(max_length=80)
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  subtitle = models.CharField(max_length=40)
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subtitle = models.CharField(max_length=40)
+    like_user = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_user"
+    )
