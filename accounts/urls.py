6cfa7ef80dc57821ad787c89310f935178de965c
JungAh Choi <jungahchoi92@gmail.com>
2022-10-28 11:17:37 +0900

    function: accounts-follow

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("", views.index, name="index"),
+    path("<int:user_pk>/follow/", views.follow, name="follow"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
@@ -18,6 +19,11 @@ def signup(request):
         "forms": forms,
     }
     return render(request, "accounts/signup.html", context)
+
+
+def index(request):
+    members = get_user_model().objects.all()
+    return render(request, "accounts/index.html")
 
 
 def login(request):
@@ -36,3 +42,13 @@ def logout(request):
     auth_logout(request)
     messages.warning(request, "로그아웃 되었습니다")
     return redirect("accounts:index")
+
+
+def follow(request, user_pk):
+    person = get_user_model().objects.get(pk=user_pk)
+    if person != request.user:
+        if person.followers.filter(pk=request.user.pk).exits():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+        return redirect("accounts:detail", user_pk)
